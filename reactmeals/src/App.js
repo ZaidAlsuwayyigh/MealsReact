cd7e16adc35fee028b3cea86c0bf29361a86924a
Zaid Alsuwayyigh <z.alsuwayyigh-t@tamkeentech.sa>
2021-08-26 21:02:06 +0300

    Added a header and a cart header

--- reactmeals/src/App.js
+++ reactmeals/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from develop</h1>
+    <div>
+      <Header />
     </div>
   );
 }
