a116ee78c1009368167ad5f8a546eabfc3509ffd
Zaid Alsuwayyigh <z.alsuwayyigh-t@tamkeentech.sa>
2021-08-30 21:23:03 +0300

    Adding a 'Meals' Component

---
+++ reactmeals/src/components/Meals/Meals.js
@@ -0,0 +1,17 @@
+import React, { Fragment} from 'react';
+import AvailableMeals from './AvaliableMeals';
+import MealsSummary from './MealsSummary';
+
+
+
+const Meals = () => {
+
+    return (
+       <Fragment>
+           <MealsSummary/>
+           <AvailableMeals />
+       </Fragment>
+    )
+}
+
+export default Meals;
